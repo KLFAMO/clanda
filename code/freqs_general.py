69e80b5d87267dedbefe6e3d5c62642e5de6b89b
Piotr Morzynski <pmorzynski@umk.pl>
2025-06-23 00:37:37 +0200

    some cleaning

---
+++ code/freqs_general.py
@@ -0,0 +1,72 @@
+
+# from FAMO_tools import fabsSr1_ml1_vs_aos_range
+from famo_config import fth698_Sr88
+from rm_data import rm_periods
+from get_data_sr_link import get_data
+
+gts = get_data(
+    included_tables={
+        # "sr1_ml1_f": {'sh_s':0, 'tol_s':5},
+        "sr1_ml1_probL": {'sh_s':0, 'tol_s':5},
+        "sr1_ml1_atomsL": {'sh_s':0, 'tol_s':5},
+        # "sr1_698_PD_ampl": {'sh_s':0, 'tol_s':5},
+        # "comb2_f_avg_counter0": {'sh_s':0.05, 'tol_s':0},
+        # "comb2_f_avg_counter5": {'sh_s':0.05, 'tol_s':0},
+        # "comb2_f_avg_counter6": {'sh_s':0.05, 'tol_s':0},
+    },
+    days=(60751, 60795),
+    rm_link_err=False,
+    rm_sr_err=True,
+)
+
+# ptb_freq = 194_399_987_408_400 
+# nc_rls = 168.5e6
+
+# gts.math_mts_and_number('add', 'comb2_f_avg_counter0', 980e6, 'frtmp')
+# gts.math_mts_and_number('divide', 'frtmp', 4, 'fr')
+# gts.math_mts_and_number('multiply', 'sr1_ml1_f', -4, 'fc')
+# N = 1_716_959.
+# gts.math_mts_and_number('multiply', 'fr', N, 'ft1')  # <----
+# gts.math_mts_and_number('add', 'ft1', -14e6-fth698_Sr88, 'ft')
+# gts.math_mts_and_mts('add', 'fc', 'comb2_f_avg_counter5', 'ftmp')
+# gts.add_mts_to_mts('ftmp', 'ft', 'fsrnc')
+# gts.add_mts_to_mts('fsrnc', 'cor', 'fsr')
+# # calc tr.osc - clean beat
+# gts.math_mts_and_number('multiply', 'comb2_f_avg_counter6', -1, 'mc6')
+# gts.math_mts_and_mts('add', 'mc6', 'comb2_f_avg_counter5', 'difc56')
+
+# # gts.rm_outlayers('sr1_ml1_atomsL', target=6000, maxdiff=2300)
+# gts.rm_outlayers('difc56', target=0, maxdiff=0.02)
+# gts.rm_outlayers('sr1_ml1_probL', target=0, maxdiff=2)
+
+# # gts.mts_dict['fsr'].rm_drift()
+# # gts.rm_outlayers('fsr')
+# gts.append_mtserie(
+#     mts_name='rfsr',
+#     mts=gts.mts_dict['fsr'].resample(period_s=600)
+# )
+# gts.append_mtserie(
+#     mts_name='rpd',
+#     mts=gts.mts_dict['sr1_698_PD_ampl'].resample(period_s=600)
+# )
+
+# # gts.append_mtserie(
+# #     mts_name='rfr',
+# #     mts=gts.mts_dict['fr'].resample(period_s=600)
+# # )
+
+gts.plot(mts_names=[
+    'sr1_ml1_atomsL',
+    'sr1_ml1_probL',
+    # 'difc56',
+    # 'rfsr',
+    # 'rpd',
+    # 'fsr',
+    # 'sr1_698_PD_ampl',
+    # 'cor',
+])
+# gts.mts_dict['fsr'].rm_drift()
+# gts.mts_dict['rfsr'].rm_drift()
+# gts.mts_dict['rfsr'].plot()
+gts.mts_dict['fsr'].plot_allan(atom='88Sr', rate=1, allan_method='adev')
+# print(gts.mts_dict['fsr'].mean())
--- code/get_data_sr_link.py
+++ code/get_data_sr_link.py
@@ -1,6 +1,7 @@
 from timanda.tserie import TSerie, MTSerie, GTserie
 from rm_data import rm_periods, rm_periods_link, rm_periods_ml
 
+# Correction data for the 698 Sr clock
 cor = [
     [60751, 263.798],
     [60753, 263.798],
@@ -19,29 +20,7 @@ cor = [
 cor_mjd = [x[0] for x in cor]
 cor_val = [x[1] for x in cor]
 
-# cor_mjd = [
-#     60751,   60753,   60753.9426,   60754.,  60755,   60763.9,      60764.5749, 60765.45, 60766
-# ]
-# cor_val = [
-#     263.798, 263.798, 99.078,       99.078,  99.078,  99.078-1.2,     44.566,       44.566,    44.566,
-# ] 
 cor_val = [x + 21.72 for x in cor_val]  # to get the same offset as in the data files
-
-
-included_tables = {
-    "sr1_ml1_f": {'sh_s':0, 'tol_s':5},
-    "sr1_ml1_probL": {'sh_s':0, 'tol_s':5},
-    "sr1_ml1_atomsL": {'sh_s':0, 'tol_s':5},
-    # "sr1_ml2_f",
-    # "sr1_ml2_probL",
-    # "sr1_ml2_atomsL",
-    # "wm_813_f": {'sh_s':0, 'tol_s':600},
-    "comb2_f_avg_counter0": {'sh_s':0.05, 'tol_s':0},
-    # "comb2_f_avg_counter3",
-    "comb2_f_avg_counter5": {'sh_s':0.05, 'tol_s':0},
-    "comb2_f_avg_counter6": {'sh_s':0.05, 'tol_s':0},
-    "comb2_f_avg_counter7": {'sh_s':0.05, 'tol_s':0},
-}
 
 def get_data(
         included_tables, days=(60751, 60765),
@@ -59,12 +38,16 @@ def get_data(
         stop_mjd=days[0]+days_range,
     )
 
+    # create grid of all data
     gts = GTserie("d")
     common_rm_mask = None
     for table in included_tables:
+        # TODO: this import and resampling should be done once and saved to pickle file
         d = MTSerie()
+        # import data from files for all days for the given table
         for day in range(days[0], days[1]+1):
             d.add_mjdf_from_datfile(f'../data_files/export_{day}/{table}.csv', delimiter=',', skiprows=1)
+        # resample data to 1 second period
         d, rm_mask = d.resample2(
             period_s=1, sh_s=included_tables[table]['sh_s'],
             tol_s=included_tables[table]['tol_s'],
@@ -74,7 +57,7 @@ def get_data(
         common_rm_mask = common_rm_mask | rm_mask if common_rm_mask is not None else rm_mask
         gts.append_mtserie(mts_name=table, mts=d)
 
-    # add ptb data
+    # add ptb data ------
     # d = MTSerie()
     # for day in range(days[0], days[1]+1):
     #     d.add_mjdf_from_datfile(f'data_files/export_{day}/ptb.dat', delimiter='\t', skiprows=3)
@@ -86,8 +69,9 @@ def get_data(
     # )
     # common_rm_mask = common_rm_mask | rm_mask if common_rm_mask is not None else rm_mask
     # gts.append_mtserie(mts_name='ptb', mts=d)
-    # -----------
+    # -------------------
     
+    # add correction data for the 698 Sr clock
     gts.append_mtserie(mts_name='cor', mts=cor)
 
     for table in included_tables:
