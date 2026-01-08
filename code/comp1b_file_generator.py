bcf877429be2f0f976bab68d23d798358364663a
piotr.morzynski <pmorzynski@umk.pl>
2026-01-08 10:16:04 +0100

    adding tmp comparator in ptb link

---
+++ code/comp1b_file_generator.py
@@ -0,0 +1,31 @@
+# comparator link PTB_NIRP-PTB_RLS
+
+# PTB_NIRP-FAMO_RLS50T -> fixed frequency shift +15 MHz  (od Jochena)
+# schemat  NIRP - FAMO_RLS = 15 MHz
+
+import astropy.time as ast
+import numpy as np
+
+def MJD2UTC(mjd, strfmt='%Y-%m-%d %H:%M:%S'):
+    t = ast.Time(mjd, format='mjd')
+    return t.strftime(strfmt)
+
+mjd = 60764
+date_str = MJD2UTC(mjd, strfmt='%Y-%m-%d')
+
+start_mjd = mjd
+stop_mjd = mjd+1
+period_mjd = 1/24/60/60  # 1 second
+nm = np.arange(start_mjd, stop_mjd, period_mjd)
+
+shift = 15e6
+
+with open(f"../share_files/PTB_NIRP-PTB_RLS/{date_str}_PTB_NIRP-PTB_RLS.dat", 'w') as f:
+    # f.write(f"# MJD\tA-B\tvalidity\n")
+    
+    for mjd in nm:
+        f.write(f"{mjd:.6f}")
+        f.write(f"\t{shift:.6f}")
+        f.write(f"\t1")
+        f.write('\n')
+f.close()
