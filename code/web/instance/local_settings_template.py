d73edf65702aa3a0e992d58b7000f04693028306
piotr.morzynski <pmorzynski@umk.pl>
2026-01-13 16:15:20 +0100

    creating local settings template

---
+++ code/web/instance/local_settings_template.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Copy this file as: local_settings.py and fill in the paths.
+
+CMPA_DATA_DIR = Path("/ABSOLUTE/PATH/TO/tock_data").resolve()
+
+# Optional settings:
+# DEBUG = True
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
